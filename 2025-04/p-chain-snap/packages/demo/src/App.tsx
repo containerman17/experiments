2d4fbbbca955489a9332210ea4a217c1178bf2bf
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-04-25 06:18:28 +0000

    hello world snap

--- 2025-04/p-chain-snap/packages/demo/src/App.tsx
+++ 2025-04/p-chain-snap/packages/demo/src/App.tsx
@@ -1,14 +1,68 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+declare global {
+    interface Window {
+        ethereum: any
+    }
+}
 
 function App() {
+    const [localError, setLocalError] = useState<string | null>(null)
+    const [isConnected, setIsConnected] = useState<boolean>(false)
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    async function connect() {
+        try {
+            await window.ethereum.request({
+                method: "eth_requestAccounts",
+            })
+            setIsConnected(true)
+        } catch (error) {
+            setLocalError((error as Error)?.message || "Unknown error")
+        }
+    }
 
-    </>
-  )
+    async function requestSnaps() {
+        try {
+            await window.ethereum.request({
+                method: "wallet_requestSnaps",
+                params: {
+                    "local:http://localhost:8080": {},
+                },
+            })
+        } catch (error) {
+            setLocalError((error as Error)?.message || "Unknown error")
+        }
+    }
+
+    if (!window.ethereum) return <div>No Ethereum provider found</div>
+
+    if (localError) return <div className="text-red-500">{localError}</div>
+
+    if (!isConnected) return <button onClick={connect}>Connect</button>
+
+    async function callHelloSnap() {
+        window.ethereum.request({
+            method: "wallet_snap",
+            params: {
+                snapId: "local:http://localhost:8080",
+                request: {
+                    method: "hello",
+                },
+            },
+        }).then((response: any) => {
+            console.log(response)
+        }).catch((error: Error) => {
+            setLocalError(error?.message || "Unknown error")
+        })
+    }
+
+    return (
+        <>
+            <button onClick={requestSnaps}>Request Snaps</button>
+            <button onClick={callHelloSnap}>Call Hello Snap</button>
+        </>
+    )
 }
 
 export default App

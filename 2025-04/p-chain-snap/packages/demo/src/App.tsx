225defd2afea41a1988360c45dcc69325e23f52f
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-04-28 04:09:14 +0000

    Get Account PubKey

--- 2025-04/p-chain-snap/packages/demo/src/App.tsx
+++ 2025-04/p-chain-snap/packages/demo/src/App.tsx
@@ -1,15 +1,55 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 declare global {
+    // Define the known request types for the snap
+    type SnapRequestMethod =
+        | { method: 'avalanche_getAccountPubKey'; params?: undefined }
+        | {
+            method: 'avalanche_sendTransaction';
+            params: {
+                transactionHex: string,
+                chainAlias: "X" | "P" | "C",
+                externalIndices?: number[],
+                internalIndices?: number[],
+                utxos?: string[],
+                feeTolerance?: number
+            }
+        }
+
+    // Define the response type based on the request method using conditional types
+    type SnapRequestResponse<T extends SnapRequestMethod> =
+        T extends { method: 'avalanche_getAccountPubKey' } ? { xp: string; evm: string } :
+        T extends { method: 'avalanche_sendTransaction' } ? { txHash: string } :
+        never;
+
     interface Window {
-        ethereum: any
+        ethereum: {
+            request(args: { method: "eth_requestAccounts" }): Promise<string[]>;
+            request(args: {
+                method: "wallet_requestSnaps";
+                params: Record<string, unknown>;
+            }): Promise<Record<string, unknown>>;
+            // Updated wallet_snap definition using generics and conditional types
+            request<T extends SnapRequestMethod>(args: {
+                method: "wallet_snap";
+                params: { snapId: string; request: T };
+            }): Promise<SnapRequestResponse<T>>;
+        }
     }
+}
+
+const Button = ({ onClick, children }: { onClick: () => void, children: React.ReactNode }) => {
+    return <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4 mb-4 cursor-pointer" onClick={onClick}>{children}</button>
 }
 
 function App() {
     const [localError, setLocalError] = useState<string | null>(null)
     const [isConnected, setIsConnected] = useState<boolean>(false)
+    const [pubKey, setPubKey] = useState<string | null>(null)
+
+    useEffect(() => {
+        connect().then(getAccountPubKey)
+    }, [])
 
     async function connect() {
         try {
@@ -22,7 +62,7 @@ function App() {
         }
     }
 
-    async function requestSnaps() {
+    async function reinstallSnap() {
         try {
             await window.ethereum.request({
                 method: "wallet_requestSnaps",
@@ -37,30 +77,40 @@ function App() {
 
     if (!window.ethereum) return <div>No Ethereum provider found</div>
 
-    if (localError) return <div className="text-red-500">{localError}</div>
+    if (!isConnected) return <div className="m-4"><Button onClick={connect}>Connect</Button></div>
 
-    if (!isConnected) return <button onClick={connect}>Connect</button>
 
-    async function callHelloSnap() {
+    async function getAccountPubKey() {
         window.ethereum.request({
             method: "wallet_snap",
             params: {
                 snapId: "local:http://localhost:8080",
                 request: {
-                    method: "hello",
+                    method: "avalanche_getAccountPubKey",
                 },
             },
-        }).then((response: any) => {
-            console.log(response)
-        }).catch((error: Error) => {
-            setLocalError(error?.message || "Unknown error")
         })
+            .then((response) => {
+                setPubKey(response.xp)
+            })
+            .catch((error: Error) => {
+                setLocalError(error?.message || "Unknown error")
+            })
+    }
+
+    async function sendTransaction() {
+        throw new Error("Not implemented")
     }
 
     return (
         <>
-            <button onClick={requestSnaps}>Request Snaps</button>
-            <button onClick={callHelloSnap}>Call Hello Snap</button>
+            <div className="m-4">
+                {localError && <div className="text-red-500 my-4">{localError}</div>}
+                <Button onClick={reinstallSnap}>Reinstall Snap</Button>
+                <Button onClick={getAccountPubKey}>Get Account PubKey</Button>
+                <Button onClick={sendTransaction}>Send Transaction</Button>
+                <div className="m-4">PubKey: {pubKey}</div>
+            </div>
         </>
     )
 }
--- 2025-04/p-chain-snap/packages/snap/src/index.tsx
+++ 2025-04/p-chain-snap/packages/snap/src/index.tsx
@@ -5,20 +5,20 @@ import nacl from 'tweetnacl';
 import { secp256k1, UnsignedTx, utils } from '@avalabs/avalanchejs';
 
 const addSigToAllCreds = async (
-  unsignedTx: UnsignedTx,
-  privateKey: Uint8Array,
+    unsignedTx: UnsignedTx,
+    privateKey: Uint8Array,
 ) => {
-  const unsignedBytes = unsignedTx.toBytes();
-  const publicKey = secp256k1.getPublicKey(privateKey);
+    const unsignedBytes = unsignedTx.toBytes();
+    const publicKey = secp256k1.getPublicKey(privateKey);
 
-  if (!unsignedTx.hasPubkey(publicKey)) {
-    return;
-  }
-  const signature = await secp256k1.sign(unsignedBytes, privateKey);
+    if (!unsignedTx.hasPubkey(publicKey)) {
+        return;
+    }
+    const signature = await secp256k1.sign(unsignedBytes, privateKey);
 
-  for (let i = 0; i < unsignedTx.getCredentials().length; i++) {
-    unsignedTx.addSignatureAt(signature, i, 0);
-  }
+    for (let i = 0; i < unsignedTx.getCredentials().length; i++) {
+        unsignedTx.addSignatureAt(signature, i, 0);
+    }
 };
 
 /**
@@ -32,70 +32,89 @@ const addSigToAllCreds = async (
  * @throws If the request method is not valid for this snap.
  */
 export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
+    origin,
+    request,
 }) => {
-  const node = await SLIP10Node.fromJSON(await snap.request({
-    method: 'snap_getBip32Entropy',
-    params: {
-      path: [`m`, `44'`, `9000'`, `0'`],
-      curve: 'secp256k1'
-    }
-  }));
-
-  const secp256k1PrivateKeyBytes = node.privateKeyBytes;
-
-  if (!secp256k1PrivateKeyBytes) {
-    throw new Error("Failed to get private key bytes");
-  }
-
-  // Use the avalanchejs library directly for secp256k1 operations
-  const secp256k1PublicKeyBytes = secp256k1.getPublicKey(secp256k1PrivateKeyBytes);
-  const address = utils.formatBech32(
-    'fuji',
-    secp256k1.publicKeyBytesToAddress(secp256k1PublicKeyBytes),
-  );
-
-  console.log('secp256k1 Private Key:', utils.bufferToHex(secp256k1PrivateKeyBytes));
-  console.log('secp256k1 Public Key:', utils.bufferToHex(secp256k1PublicKeyBytes));
-  console.log('Derived Address:', address);
-
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
+    const { privateKeyBytes } = await SLIP10Node.fromJSON(await snap.request({
+        method: 'snap_getBip32Entropy',
         params: {
-          type: 'confirmation',
-          content: (
-            <Box>
-              <Text>
-                PrivateKey: {utils.bufferToHex(secp256k1PrivateKeyBytes)}
-              </Text>
-              <Text>
-                PublicKey: {utils.bufferToHex(secp256k1PublicKeyBytes)}
-              </Text>
-              <Text>
-                Address: {address}
-              </Text>
-              <Text>
-                Expected address is fuji15dhs4ee8cahe52rdslnerpm3an64fsd6qwrztm
-              </Text>
+            path: [`m`, `44'`, `9000'`, `0'`],
+            curve: 'secp256k1'
+        }
+    }));
 
-              <Text>
-                Hello, <Bold>{origin}</Bold>!
-              </Text>
-              <Text>
-                This custom confirmation is just for display purposes.
-              </Text>
-              <Text>
-                But you can edit the snap source code to make it do something,
-                if you want to!
-              </Text>
+    if (!privateKeyBytes) {
+        throw new Error('Failed to get private key bytes');
+    }
+
+    // Use the avalanchejs library directly for secp256k1 operations
+    const secp256k1PublicKeyBytes = secp256k1.getPublicKey(privateKeyBytes);
+    const address = utils.formatBech32(
+        'fuji',
+        secp256k1.publicKeyBytesToAddress(secp256k1PublicKeyBytes),
+    );
+
+    console.log('secp256k1 Private Key:', utils.bufferToHex(privateKeyBytes));
+    console.log('secp256k1 Public Key:', utils.bufferToHex(secp256k1PublicKeyBytes));
+    console.log('Derived Address:', address);
+
+    switch (request.method) {
+        case 'avalanche_getAccountPubKey':
+            return {
+                xp: utils.bufferToHex(secp256k1PublicKeyBytes), // No 0x prefix as requested
+                evm: ""
+            };
+        case 'avalanche_sendTransaction':
+            console.log('avalanche_sendTransaction', request);
+
+            return snap.request({
+                method: 'snap_dialog',
+                params: {
+                    type: 'confirmation',
+                    content: (
+                        <Box>
+                            <Text>
+                                PrivateKey: {utils.bufferToHex(privateKeyBytes)}
+                            </Text>
+                            <Text>
+                                PublicKey: {utils.bufferToHex(secp256k1PublicKeyBytes)}
+                            </Text>
+                            <Text>
+                                Address: {address}
+                            </Text>
+                            <Text>
+                                Expected address is fuji15dhs4ee8cahe52rdslnerpm3an64fsd6qwrztm
+                            </Text>
+
+                            <Text>
+                                Hello, <Bold>{origin}</Bold>!
+                            </Text>
+                            <Text>
+                                This custom confirmation is just for display purposes.
+                            </Text>
+                            <Text>
+                                But you can edit the snap source code to make it do something,
+                                if you want to!
+                            </Text>
+                        </Box>
+                    ),
+                },
+            });
+        default:
+            throw new Error('Method not found.');
+    }
+};
+
+import type { OnHomePageHandler } from "@metamask/snaps-sdk";
+import { Heading } from "@metamask/snaps-sdk/jsx";
+
+export const onHomePage: OnHomePageHandler = async () => {
+    return {
+        content: (
+            <Box>
+                <Heading>Hello world!</Heading>
+                <Text>Welcome to my Snap home page!</Text>
             </Box>
-          ),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
+        ),
+    };
 };

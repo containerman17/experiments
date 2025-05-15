46548c99cdd12f31adfaabd6781bbcd4ac2e7ef9
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-05-15 16:27:19 +0900

    simple tx

--- 2025-05/06_as_proxy/server/indexer.ts
+++ 2025-05/06_as_proxy/server/indexer.ts
@@ -2,7 +2,26 @@ import { RPC } from "./rpc/rpc.ts"
 import dotenv from "dotenv"
 import { S3BlockStore } from "./rpc/s3cache.ts";
 import { CachedRPC } from "./rpc/cachedRpc.ts";
+import { toBytes } from 'viem'
+import Database from 'better-sqlite3';
+import { StoredBlock } from "./rpc/types.ts";
+const db = new Database('/tmp/foobar.db');
+db.pragma('journal_mode = WAL');
+
+db.exec('CREATE TABLE IF NOT EXISTS tx_block_lookup (hash_to_block BLOB PRIMARY KEY) WITHOUT ROWID;');
+db.exec('CREATE TABLE IF NOT EXISTS configs (key TEXT PRIMARY KEY, value TEXT)');
+
 dotenv.config()
+
+//This function is guaranteed to be called in order and inside a transaction
+function handleBlock({ block, receipts }: StoredBlock) {
+    console.log(`Block ${Number(block.number)} has ${Object.keys(receipts).length} receipts`)
+    for (const tx of block.transactions) {
+        const txHashBytes = toBytes(tx.hash)
+        const lookupKey = Buffer.from([...txHashBytes.slice(0, 5), ...toBytes(Number(block.number))])
+        db.prepare('INSERT INTO tx_block_lookup (hash_to_block) VALUES (?)').run(lookupKey)
+    }
+}
 
 async function startLoop() {
     const uncachedRPC = new RPC(process.env.RPC_URL!);
@@ -10,12 +29,15 @@ async function startLoop() {
     const cacher = new S3BlockStore(chainIdbase58)
     const cachedRPC = new CachedRPC(cacher, uncachedRPC)
 
-    let currentBlock = 0
-    for (let i = 0; i < 20; i++) {
-        console.time(`getBlock ${currentBlock}`)
+    let currentBlock = 100
+
+    for (let i = currentBlock; i < (260); i++) {
         const block = await cachedRPC.getBlock(currentBlock)
-        console.timeEnd(`getBlock ${currentBlock}`)
         currentBlock++
+        const runTx = db.transaction((cats) => {
+            handleBlock(block)
+        });
+        runTx()
     }
 }
 

1b6c2387832fd965f34446b2ec906116f3b0c7df
containerman17 <8990432+containerman17@users.noreply.github.com>
2026-01-16 23:39:45 +0000

    wip

--- 2025-08/08_node_versions/deploy.sh
+++ 2025-08/08_node_versions/deploy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # fly app create l1-validator-discovery-mainnet
 # fly app create l1-validator-discovery-fuji
 
-go get github.com/ava-labs/avalanchego@v1.13.5
+go get github.com/ava-labs/avalanchego@v1.14.0
 go mod tidy
 fly secrets set -a l1-validator-discovery-mainnet AVA_NETWORK=mainnet
 fly deploy --app l1-validator-discovery-mainnet --primary-region iad --local-only

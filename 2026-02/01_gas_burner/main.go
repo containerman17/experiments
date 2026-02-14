f3eb53266e6cab6b46c9aa2fc2f26bde0116e240
containerman17 <8990432+containerman17@users.noreply.github.com>
2026-02-14 02:01:27 +0000

    aggr

--- 2026-02/01_gas_burner/main.go
+++ 2026-02/01_gas_burner/main.go
@@ -354,13 +354,11 @@ func burnLoop(
 		var baseGas uint64
 		switch {
 		case ratio >= 0.95:
-			baseGas = 1_000_000
-		case ratio >= 0.90:
-			baseGas = 2_000_000
-		case ratio >= 0.85:
 			baseGas = 4_000_000
-		case ratio >= 0.80:
+		case ratio >= 0.90:
 			baseGas = 8_000_000
+		case ratio >= 0.85:
+			baseGas = 16_000_000
 		default:
 			baseGas = 16_000_000
 		}
